ba809cac49de83ebdcb319a852f37cbc898a352d
Stas <staszhingel@icloud.com>
2021-01-08 21:38:07 +0300

    Initial Commit

---
+++ Facebook- layout/Facebook__layoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  Facebook__layoutApp.swift
+//  Facebook- layout
+//
+//  Created by Стас Жингель on 08.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct Facebook__layoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
